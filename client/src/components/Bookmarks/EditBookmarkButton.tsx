7fb47467868fa82821cfb1826d052d19ff5f95e2
Marco Beretta <81851188+berry-13@users.noreply.github.com>
2024-08-22 15:26:44 +0200

    inital Tooltip implementation and test

--- client/src/components/Bookmarks/EditBookmarkButton.tsx
+++ client/src/components/Bookmarks/EditBookmarkButton.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import type { FC } from 'react';
 import type { TConversationTag } from 'librechat-data-provider';
 import BookmarkEditDialog from './BookmarkEditDialog';
+import { TooltipAnchor } from '~/components/ui';
 import { EditIcon } from '~/components/svg';
 import { useLocalize } from '~/hooks';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui';
 
 const EditBookmarkButton: FC<{
   bookmark: TConversationTag;
@@ -17,31 +17,19 @@ const EditBookmarkButton: FC<{
 
   return (
     <>
-      <BookmarkEditDialog
-        context="EditBookmarkButton"
-        bookmark={bookmark}
-        open={open}
-        setOpen={setOpen}
-      />
-      <button
-        type="button"
-        className="transition-color flex size-7 items-center justify-center rounded-lg duration-200 hover:bg-surface-hover"
-        tabIndex={tabIndex}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onClick={() => setOpen(!open)}
-      >
-        <TooltipProvider delayDuration={250}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <EditIcon />
-            </TooltipTrigger>
-            <TooltipContent side="top" sideOffset={0}>
-              {localize('com_ui_edit')}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </button>
+      <BookmarkEditDialog context="EditBookmarkButton" bookmark={bookmark} open={open} setOpen={setOpen} />
+      <TooltipAnchor description={localize('com_ui_edit')}>
+        <button
+          type="button"
+          className="transition-color flex size-7 items-center justify-center rounded-lg duration-200 hover:bg-surface-hover"
+          tabIndex={tabIndex}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onClick={() => setOpen(!open)}
+        >
+          <EditIcon />
+        </button>
+      </TooltipAnchor>
     </>
   );
 };
--- client/src/components/Chat/PromptLanding.tsx
+++ client/src/components/Chat/PromptLanding.tsx
@@ -2,7 +2,6 @@ import { useGetEndpointsQuery } from 'librechat-data-provider/react-query';
 import { EModelEndpoint, isAssistantsEndpoint } from 'librechat-data-provider';
 import type { ReactNode } from 'react';
 import { useChatContext, useAssistantsMapContext } from '~/Providers';
-import { TooltipProvider, Tooltip } from '~/components/ui';
 import ConvoIcon from '~/components/Endpoints/ConvoIcon';
 import { getIconEndpoint, cn } from '~/utils';
 import Prompts from './Prompts';
@@ -28,35 +27,31 @@ export default function Landing({ Header }: { Header?: ReactNode }) {
 
   const isAssistant = isAssistantsEndpoint(endpoint);
   const assistant = isAssistant && assistantMap?.[endpoint]?.[assistant_id ?? ''];
-  const assistantName = (assistant && assistant?.name) || '';
-  const avatar = (assistant && (assistant?.metadata?.avatar as string)) || '';
+  const assistantName = (assistant && assistant.name) || '';
+  const avatar = (assistant && (assistant.metadata?.avatar as string)) || '';
 
   const containerClassName =
     'shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black';
 
   return (
-    <TooltipProvider delayDuration={50}>
-      <Tooltip>
-        <div className="relative h-full">
-          <div className="absolute left-0 right-0">{Header && Header}</div>
-          <div className="flex h-full flex-col items-center justify-center">
-            <div className={cn('relative h-12 w-12', assistantName && avatar ? 'mb-0' : 'mb-3')}>
-              <ConvoIcon
-                conversation={conversation}
-                assistantMap={assistantMap}
-                endpointsConfig={endpointsConfig}
-                containerClassName={containerClassName}
-                context="landing"
-                className="h-2/3 w-2/3"
-                size={41}
-              />
-            </div>
-            <div className="h-3/5">
-              <Prompts />
-            </div>
-          </div>
+    <div className="relative h-full">
+      <div className="absolute left-0 right-0">{Header && Header}</div>
+      <div className="flex h-full flex-col items-center justify-center">
+        <div className={cn('relative h-12 w-12', assistantName && avatar ? 'mb-0' : 'mb-3')}>
+          <ConvoIcon
+            conversation={conversation}
+            assistantMap={assistantMap}
+            endpointsConfig={endpointsConfig}
+            containerClassName={containerClassName}
+            context="landing"
+            className="h-2/3 w-2/3"
+            size={41}
+          />
         </div>
-      </Tooltip>
-    </TooltipProvider>
+        <div className="h-3/5">
+          <Prompts />
+        </div>
+      </div>
+    </div>
   );
 }
